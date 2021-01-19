1a3781e945288c6e884189bfeceb402f15f47648
Сергей Александрович <serega-aleks23@outlook.com>
2021-01-19 17:53:32 +0300

    Work on project. Stage 5/6: Keep track of the supplies
    But just one action isn’t interesting. Let's improve the program so it can do multiple actions, one after another. The program should repeatedly ask what the user wants to do. If the user types "buy", "fill" or "take", then just do what the program did in the previous step. However, if the user wants to switch off the coffee machine, he should type "exit". Then the program should terminate. Also, when the user types "remaining", the program should output all the resources that the coffee machine has.
    
    Also, do not forget that you can be out of resources for making coffee. If the coffee machine doesn’t have enough resources to make coffee, the program should output a message that says it can't make a cup of coffee.
    
    And the last improvement to the program at this step—if the user types "buy" to buy a cup of coffee and then changes his mind, he should be able to type "back" to return into the main cycle.

--- Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,54 +3,140 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+    static int water = 400;
+    static int milk = 540;
+    static int coffeeBeans = 120;
+    static int cup = 9;
+    static int money = 550;
+
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        int water = SetWater(scanner);
-        int milk  = SetMilk(scanner);
-        int coffee = SetCoffee(scanner);
-        int cup = SetCup(scanner);
-        int makeCup = GetMakeCup(water,milk, coffee);
-        if(cup == makeCup)
-            System.out.println("Yes, I can make that amount of coffee");
-        else if (cup > makeCup)
-            System.out.println("No, I can make only "+makeCup+" cup(s) of coffee");
-        else
-            System.out.println("Yes, I can make that amount of coffee (and even "+ (makeCup-cup) +" more than that)");
-
-
+        Process();
+    }
+    static void Process() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nWrite action (buy, fill, take, remaining, exit): ");
+            String str = scanner.nextLine();
+            if(str.equals("exit"))
+                break;
+            switch (str) {
+                case "buy":
+                    OnBuy();
+                    break;
+                case "fill":
+                    OnFill();
+                    break;
+                case "take":
+                    OnTake();
+                    break;
+                case "remaining":
+                    OnRemaining();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    static int GetMakeCup(int water, int milk, int coffee){
-        water = water / GetWater(1);
-        milk = milk / GetMilk(1);
-        coffee = coffee / GetCoffee(1);
-
-        return  Math.min(Math.min(water, milk), coffee);
+    static void OnRemaining(){
+        System.out.println(GetStatus());
     }
 
-    static int GetWater(int cup){
-        return 200 * cup;
+    static void OnTake(){
+        System.out.println("I gave you $" + money);
+        money = 0;
+        System.out.println(GetStatus());
     }
-    static int GetMilk(int cup){
-        return 50 * cup;
+
+    static  void OnFill(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water do you want to add:");
+        water = water + scanner.nextInt();
+        System.out.println("Write how many ml of milk do you want to add:");
+        milk = milk + scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans do you want to add:");
+        coffeeBeans = coffeeBeans + scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee do you want to add:");
+        cup = cup + scanner.nextInt();
     }
-    static int GetCoffee(int cup){
-        return  15 * cup;
+    static void OnBuy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String command = scanner.next();
+        if(command.length() == 1) {
+            int typeCoffee = Character.getNumericValue(command.charAt(0));
+            if ( water < GetWater(1, typeCoffee) )
+                System.out.println("Sorry, not enough water!");
+            else if ( milk < GetMilk(1, typeCoffee) )
+                System.out.println("Sorry, not enough milk!");
+            else if ( coffeeBeans < GetCoffeeBeans(1, typeCoffee) )
+                System.out.println("Sorry, not enough coffee beans!");
+            else if ( cup == 0 )
+                System.out.println("Sorry, not enough disposable cups!");
+            else {
+                water = water - GetWater(1, typeCoffee);
+                milk = milk - GetMilk(1, typeCoffee);
+                coffeeBeans = coffeeBeans - GetCoffeeBeans(1, typeCoffee);
+                cup = cup - 1;
+                money = money + GetMoney(1, typeCoffee);
+                System.out.println("I have enough resources, making you a coffee!");
+            }
+        }
     }
-    static int SetWater(Scanner scanner){
-        System.out.println("Write how many ml of water the coffee machine has:");
-        return scanner.nextInt();
+
+    static String GetStatus(){
+        return "\nThe coffee machine has:\n" +
+                water + " of water\n" +
+                milk + " of milk\n" +
+                coffeeBeans + " of coffee beans\n" +
+                cup + " of disposable cups\n" +
+                "$"+money + " of money";
     }
-    static int SetMilk(Scanner scanner){
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        return scanner.nextInt();
+
+    static int GetWater(int cup, int coffee) {
+        switch (coffee) {
+            case 1:
+                return 250 * cup;
+            case 2:
+                return 350 * cup;
+            case 3:
+                return 200 * cup;
+            default: return 0;
+        }
     }
-    static int SetCoffee(Scanner scanner){
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        return scanner.nextInt();
+
+    static int GetMilk(int cup, int coffee){
+        switch (coffee) {
+            case 2:
+                return 75 * cup;
+            case 3:
+                return 100 * cup;
+            default: return 0;
+        }
     }
-    static int SetCup(Scanner scanner){
-        System.out.println("Write how many cups of coffee you will need:");
-        return scanner.nextInt();
+
+    static int GetCoffeeBeans(int cup, int coffee){
+        switch (coffee) {
+            case 1:
+                return 16 * cup;
+            case 2:
+                return 20 * cup;
+            case 3:
+                return 12 * cup;
+            default: return 0;
+        }
+    }
+
+    static  int GetMoney(int cup, int coffee){
+        switch (coffee) {
+            case 1:
+                return 4 * cup;
+            case 2:
+                return 7 * cup;
+            case 3:
+                return 6 * cup;
+            default: return 0;
+        }
     }
 }
+
