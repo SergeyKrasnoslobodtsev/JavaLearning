9855c9c1445810e7a63e4b3d54bee93bb7487401
Сергей Александрович <serega-aleks23@outlook.com>
2021-02-08 13:13:01 +0300

    CoffeeMachine

--- Coffee Machine/Problems (6)/Maximum element in a matrix/src/Main.java
+++ Coffee Machine/Problems (6)/Maximum element in a matrix/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner (System.in);
+        int n = scanner.nextInt ();
+        int m = scanner.nextInt ();
+        int [] [] matrix = new int [n] [m];
+        for (int i = 0; i <n; i ++) {
+            for (int j = 0; j <m; j ++) {
+                matrix [i] [j] = scanner.nextInt ();
+            }
+        }
+        int maximum = matrix [0] [0];
+        int x = Integer.MIN_VALUE;
+        int y = Integer.MIN_VALUE;
+        for (int i = 0; i <n; i ++) {
+            for (int j = 0; j <m; j ++) {
+                if (matrix [i] [j]> maximum)
+                {
+                    maximum = matrix [i] [j] ;
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        System.out.println (x + "" + y); }
     }
 }
